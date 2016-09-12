654c71cade90dd7476a3a0d7a2c44029dae05a73
Finels Minvic <504096193@qq.com>
2016-09-12 23:41:08 +0800

    Update html_outputer.py

--- html_outputer.py
+++ html_outputer.py
@@ -1,6 +1,26 @@
 class HtmlOutputer(object):
-    def collect_data(self, new_data):
-        pass
+    '''
+    网页输出器，可以自定义输出格式
+    '''
+    def __init__(self):
+        self.datas = []
+    def collect_data(self, data):
+                if data is None:
+            return
+        self.datas.append(data)
 
     def output_html(self):
-        pass
+        fout = open('output.html', 'w')
+        fout.write("<html>")
+        fout.write("<body>")
+        fout.write("<table>")
+        for data in self.datas:
+            fout.write("<tr>")
+            fout.write("<td>%s</td>" % data['url'])
+            fout.write("<td>%s</td>" % data['title'].encode('utf-8'))
+            fout.write("<td>%s</td>" % data['summary'].encode('utf-8'))
+            fout.write("</tr>")
+
+        fout.write("</table>")
+        fout.write("</body")
+        fout.write("</html>")
